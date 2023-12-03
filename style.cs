c2dadee33091c5eda47097629398064c594c8470
Yousaf Amin Khan <151819099+YousaF459@users.noreply.github.com>
2023-12-03 23:28:51 +0500

    Css File

---
+++ style.cs
@@ -0,0 +1,77 @@
+
+
+body, html {
+  height: 100%;
+width:100%;
+margin:0;
+}
+
+
+
+
+
+
+#div1{
+	background-color:#edceea;
+	width:100%;
+	height: 100rem;
+	position: relative;
+	
+    	
+
+
+
+	
+}
+
+
+#div2{
+	background-color:white;
+	position:absolute;
+
+	
+
+	left:32.5%;
+	right:32.5%;
+	margin-top:-8.5%;
+	height:auto;
+	width:35vw;
+
+	margin-bottom: 10px;
+
+		
+	border-radius: 3%; 
+	
+	
+}
+
+
+.same-divs{
+width:85%;
+margin-top:5%;
+border-top:1px solid #ccc;
+margin-left:7%
+
+
+
+}
+
+.same-h{
+display:inline-block;
+margin-top:8%;
+font-size:20px;
+
+width:79.8%;
+
+}
+
+.same-i{
+display:inline-block;
+float:right;
+padding-top:9%;
+
+
+}
+
+
+
